4d55204fd44e1e4c06c26e758cb4fc0019cda805
Gabriele-Zini <gabrielezini845@gmail.com>
2023-11-05 15:40:46 +0100

    add: setInerval

--- js/script.js
+++ js/script.js
@@ -6,41 +6,70 @@ const images = [
   "./img/05.jpg",
 ];
 
-const slideElems = document.querySelector(".items");
+const container = document.querySelector(".container");
+const items = document.querySelector(".items");
 
-let imagesString = "";
+creatImg();
+setInterval(sliderInterval, 3000);
 
-for (let i = 0; i < images.length; i++) {
-  const curImage = images[i];
-  imagesString += `
-   <div class="item">
-    <img src="${curImage}" alt="">
-   </div>`;
+let autoplay = false;
+let currentIndex = 0;
+const imageItem = document.querySelectorAll(".item");
+imageItem[currentIndex].classList.add("active");
+
+const next = document.querySelector(".next");
+next.addEventListener("click", nextFunction);
+
+const prev = document.querySelector(".prev");
+prev.addEventListener("click", prevFunction);
+
+container.addEventListener("mouseover", function () {
+  autoplay = false;
+});
+
+container.addEventListener("mouseout", function () {
+  autoplay = true;
+});
+
+// FUNCTIONS
+
+function creatImg() {
+  let imageDiv = "";
+
+  for (let i = 0; i < images.length; i++) {
+    const curImage = images[i];
+    imageDiv += `<div class="item"><img src="${curImage}" alt=""></div>`;
+  }
+
+  items.innerHTML += imageDiv;
+  return imageDiv;
 }
 
-slideElems.innerHTML += imagesString;
-
-const slideItems = document.querySelectorAll(".item");
-
-let currentSlideIndex = 0;
-slideItems[currentSlideIndex].classList.add("active");
-
-document.querySelector(".next").addEventListener("click", function (event) {
-  event.preventDefault();
-  slideItems[currentSlideIndex].classList.remove("active");
-  currentSlideIndex++;
-  if (currentSlideIndex >= slideItems.length) {
-    currentSlideIndex = 0;
+function nextFunction() {
+  imageItem[currentIndex].classList.remove("active");
+  currentIndex++;
+  if (currentIndex >= imageItem.length) {
+    currentIndex = 0;
   }
-  slideItems[currentSlideIndex].classList.add("active");
-});
+  imageItem[currentIndex].classList.add("active");
+}
 
-document.querySelector(".prev").addEventListener("click", function (event) {
-  event.preventDefault();
-  slideItems[currentSlideIndex].classList.remove("active");
-  currentSlideIndex--;
-  if (currentSlideIndex < 0) {
-    currentSlideIndex = slideItems.length - 1;
+function prevFunction() {
+  imageItem[currentIndex].classList.remove("active");
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = imageItem.length - 1;
   }
-  slideItems[currentSlideIndex].classList.add("active");
-});
+  imageItem[currentIndex].classList.add("active");
+}
+
+function sliderInterval() {
+  if (autoplay) {
+    imageItem[currentIndex].classList.remove("active");
+    currentIndex++;
+    if (currentIndex >= imageItem.length) {
+      currentIndex = 0;
+    }
+    imageItem[currentIndex].classList.add("active");
+  }
+}
